d0f4c61216bb7fe1c170f0d3d51a7855cb2868d1
Jay Pfaffman <jay@literatecomputing.com>
2025-04-09 13:11:26 -0500

    remove debugging

--- javascripts/discourse/components/jeppseon.js
+++ javascripts/discourse/components/jeppseon.js
@@ -1,10 +1,7 @@
 import Component from "@glimmer/component";
-import Category from "discourse/models/category";
-import { i18n } from "discourse-i18n";
 
 export default class Jeppseon extends Component {
   get jLogo() {
-    console.log("jepp" , settings);
     return "loog.jpg";
   }
 }
